31b835cd607b3fd5908d47d2e5ada30d236c0b1a
Priyanshu <priyanshutripathi7777@gmail.com>
2024-03-04 13:15:57 -0500

    solve error in jsx/ts syntax

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,12 +4,17 @@ import Image from 'next/image'
 import { ArrowDownCircleIcon } from '@heroicons/react/16/solid'
 
 const Hero = () => {
+
+    const downloadCV = () => {
+        let pdf = '/pdf/resume.pdf'
+        window.open(pdf, '_blank');
+    }
     return (
         <div className='flex  items-center justify-start w-[100%] h-[80vh]'>
             <div className='flex flex-row justify-between items-center w-[100%] mx-10 '>
                 <div className=''>
                     <h1 className='text-[2rem] md:text-[3rem] font-extrabold'>
-                        Hi, I'M Priyanshu Tripathi
+                        Hi, I&apos;M Priyanshu Tripathi
                     </h1>
                     <TextAnimation />
                     <p className='w-[400px] md:w-[800px]'>
@@ -19,7 +24,7 @@ const Hero = () => {
                         facilis numquam facere assumenda non neque. Libero esse dicta animi consectetur aperiam ratione hic!
                     </p>
                    
-                    <button className='bg-blue-400 flex flex-row p-3 my-4 rounded-md items-center'>
+                    <button onClick={downloadCV} className='bg-blue-400 flex flex-row p-3 my-4 rounded-md items-center'>
                         Download CV <ArrowDownCircleIcon className="ml-2 w-6 h-6"/>
                     </button>
                 </div>
